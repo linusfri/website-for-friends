d7622461ee149e2bedfcd01c25bbf2728cbf71de
linus <linus.f@protonmail.ch>
2025-10-05 23:38:10 +0200

    Edited envs

--- config/application.php
+++ config/application.php
@@ -76,7 +76,7 @@ Config::define('WP_SITEURL', env('WP_SITEURL'));
 /**
  * Custom Content Directory
  */
-Config::define('CONTENT_DIR', '/app');
+Config::define('CONTENT_DIR', Config::get('CONTENT_PATH') ?: '/app');
 Config::define('WP_CONTENT_DIR', $webroot_dir . Config::get('CONTENT_DIR'));
 Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_DIR'));
 
