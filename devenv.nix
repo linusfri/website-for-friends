0d2241a5017868ac1e0c84b97dbf6735dbace006
linus <linus.f@protonmail.ch>
2025-10-05 19:28:01 +0200

    init

---
+++ devenv.nix
@@ -0,0 +1,40 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+let
+  php = pkgs.php83;
+
+  devDeps = {
+    inherit php;
+    inherit (pkgs) wp-cli nodejs_22;
+    inherit (php.packages) composer;
+  };
+
+  name = "bedrock";
+in
+{
+  inherit name;
+
+  # Include git in dev shell
+  packages = [ pkgs.git ] ++ builtins.attrValues devDeps;
+
+  services.linusfri = {
+    mysql = {
+      enable = true;
+      dbName = name;
+    };
+
+    phpServer = {
+      enable = true;
+      inherit php;
+      domains = [ "${name}.local" ];
+      serveDir = "/web";
+      appType = "wordpress";
+      # assetFallbackUrl = "https://www.${name}.se";
+    };
+  };
+}
